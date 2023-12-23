f15f3422f468e44649d12805883e0015db3b3c9e
Taleb Damaree <tdamaree18@gmail.com>
2023-12-23 12:37:16 -0500

    Second commit - Created new workbook that filters out non-numeric values pertaining to incident amounts

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from warnings import simplefilter # suppresses warning, allows > 100 columns to be created in new dataframe
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
@@ -17,6 +16,7 @@ teacher_out_to_field_df = pd.read_excel('23-RC-Pub-Data-Set.xlsx', sheet_name='T
 discipline_df = pd.read_excel('23-RC-Pub-Data-Set.xlsx', sheet_name='Discipline')
 
 print(general_df)
+
 # create condensed version of discipline data sheet
 discipline_condensed_df = pd.DataFrame()
 rcdts = discipline_df.iloc[:,0]
@@ -34,8 +34,10 @@ discipline_condensed_df['# of Discipline Incidents'] = \
 # delete empty rows
 def delete_row(df): # find all the non-missing values ONLY
     return df[df['School Name'].notna()]
+
 # delete rows with empty cell under School Name
 finance_condensed_df = delete_row(discipline_condensed_df)
+
 # discipline condensed excel file xlsx
 discipline_condensed_df.to_excel('discipline_condensed.xlsx', index=False)
 
@@ -87,3 +89,12 @@ discipline_finance_condensed_df = discipline_finance_condensed_df.sort_values(
 )
 
 discipline_finance_condensed_df.to_excel('discipline_finance_condensed.xlsx', index=False)
+
+def incident_amounts_provided(df):
+    # conversion of non-numeric values to NaN and then filtering rows based on those values
+    numeric_mask = pd.to_numeric(df['# of Discipline Incidents'], errors='coerce').notna()
+    return df[numeric_mask]
+
+incident_amounts_df = incident_amounts_provided(discipline_condensed_df)
+
+incident_amounts_df.to_excel('incident_amounts_provided.xlsx', index=False)
