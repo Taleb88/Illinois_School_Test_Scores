b5f7caff0ec830ff7af984724b7e4116b9a11abb
Taleb Damaree <tdamaree18@gmail.com>
2024-01-07 21:04:44 -0500

    Database testing in progress - Confirming that SQLite (sqlite3) will be used

--- main.py
+++ main.py
@@ -1209,4 +1209,4 @@ ela_only_pivot_table_df = pd.pivot_table(
 
 ela_only_pivot_table_df.to_excel('ela_proficiency_total_student_pivot_table.xlsx') # 0 value populated for preschools
 
-# DATABASE TESTING
+# DATABASE TESTING via MongoDB
