19652eb01d9385ae9f4c4b60cb13a51cd2459d72
Taleb Damaree <tdamaree18@gmail.com>
2024-01-10 23:09:57 -0500

    More docs developed relative to school types and district sizes

--- main.py
+++ main.py
@@ -1244,6 +1244,23 @@ ela_proficiency_middle_schools_small_district_size_only_df = \
 ela_proficiency_middle_schools_small_district_size_only_df.\
     to_excel('ela_proficiency_middle_schools_small_district_size_only.xlsx', index=False)
 #preschools
+ela_proficiency_preschools_large_district_size_only_df = \
+    large_district_size(ela_proficiency_preschools_only_df)
+
+ela_proficiency_preschools_large_district_size_only_df.\
+    to_excel('ela_proficiency_preschools_large_district_size_only.xlsx', index=False)
+
+ela_proficiency_preschools_medium_district_size_only_df = \
+    medium_district_size(ela_proficiency_preschools_only_df)
+
+ela_proficiency_preschools_medium_district_size_only_df.\
+    to_excel('ela_proficiency_preschools_medium_district_size_only.xlsx', index=False)
+
+ela_proficiency_preschools_small_district_size_only_df = \
+    small_district_size(ela_proficiency_preschools_only_df)
+
+ela_proficiency_preschools_small_district_size_only_df.\
+    to_excel('ela_proficiency_preschools_small_district_size_only.xlsx', index=False)
 
 # add status column to ela_proficiency_preschools_only_df and set all values to = 'N/A'
 ela_proficiency_preschools_only_df['Status'] =\
